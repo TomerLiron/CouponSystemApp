df47a53340eb4a0dbfbb050c4f01d1cb08cdede5
EvgenieTiv <80687914+EvgenieTiv@users.noreply.github.com>
2022-01-10 19:36:20 +0200

    Added all requiredf functions to AdminController
    Added all required functions to AdminController

--- cupon_server/src/main/java/com/evgenie_tomer_itay/controllers/Admin_controller.java
+++ cupon_server/src/main/java/com/evgenie_tomer_itay/controllers/Admin_controller.java
@@ -1,10 +1,17 @@
 package com.evgenie_tomer_itay.controllers;
 
 
+import com.evgenie_tomer_itay.entities.Company;
+import com.evgenie_tomer_itay.entities.Customer;
 import com.evgenie_tomer_itay.exceptions.adminExceptions.AdminNotExistsException;
 import com.evgenie_tomer_itay.services.AdminServiceImplementation;
 import com.evgenie_tomer_itay.utilities.Credentials;
+import com.evgenie_tomer_itay.utilities.ObjectToJson;
 import com.evgenie_tomer_itay.utilities.SimpleTokenManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +21,13 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/admin")
-public class Admin_controller {
 
+/**
+ * I think class name should be changed to: AdminController
+ */
+
+public class Admin_controller 
+{
     @Autowired
     AdminServiceImplementation adminService;
     @Autowired
@@ -23,103 +35,252 @@ public class Admin_controller {
 
     private final String ADMIN_EMAIL = "admin@admin.com";
     private final String ADMIN_PASSWORD = "admin";
-//    private int companyId;
 
-
+/*  
+    @GetMapping("login")
+    @ResponseBody
+    public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password)     
+    {
+    	System.out.println("Login");
+    	
+    	try 
+    	{    		
+			adminService.login(email, password);
+			
+			// String token = simpleTokenManager.getNewToken();
+			
+			return new ResponseEntity<String>("Logged correctly as Admin", HttpStatus.OK);
+			
+			// return new ResponseEntity<String>(token, HttpStatus.OK);
+		} 
+    	
+    	catch (AdminNotExistsException e) 
+    	{
+    		
+			e.printStackTrace();
+			return new ResponseEntity<String>("Log in as Admin failed", HttpStatus.BAD_REQUEST);
+		}
+    }
+    */
+    
+    
     @PostMapping("login")
-    public ResponseEntity<?> login(@RequestBody Credentials cred)  {
+    public ResponseEntity<?> login(@RequestBody Credentials cred)  
+    {
         System.out.println(new Date()+": Got a new login: "+cred);
-        try {
+        try
+        {
             adminService.login(cred.getEmail(), cred.getPassword());
             String token = simpleTokenManager.getNewToken();
             return new ResponseEntity<String>(token, HttpStatus.OK);
-        } catch (AdminNotExistsException e) {
+        } 
+        
+        catch (AdminNotExistsException e) 
+        {
             return new ResponseEntity<String>("Login error!", HttpStatus.BAD_REQUEST);
         }
     }
 
-
-    //spring security,token
-/*    @GetMapping("all")
-    public ArrayList<Map<String, String>> getAllCompany() {
-
-        List<Company> companies= companyRepository.findAll();
-        System.out.println("all " + companies.size());
-        ArrayList<Map<String, String>> Map = new ArrayList<Map<String, String>>();
-        //@RequestParam String category,@RequestParam String title,@RequestParam String description, @RequestParam int amount,
-        //@RequestParam String startDate, @RequestParam String endDate,@RequestParam double price,@RequestParam String image
-
-        for (int i = 0; i < companies.size(); i++) {
-            Map<String, String> m = new HashMap<String, String>();
-            m.put("id", "" + companies.get(i).getId());
-            m.put("name",  companies.get(i).getName());
-            m.put("email", companies.get(i).getEmail());
-            m.put("password", companies.get(i).getPassword());
-            Map.add(m);
-        }
-        return Map;
-    }*/
-
-/*
-    @GetMapping("add")
-    @ResponseBody
-    public ArrayList<Map<String, String>> addCoupon(@RequestParam String category, @RequestParam String title, @RequestParam String description, @RequestParam int amount,
-                                                    @RequestParam String startDate, @RequestParam String endDate, @RequestParam double price, @RequestParam String image) {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        LocalDate localStartDate = LocalDate.parse(startDate, formatter);
-        LocalDate localEndDate = LocalDate.parse(endDate, formatter);
-
-        Coupon coupon = Coupon.builder().category(Category.valueOf(category)).title(title).description(description).startDate(localStartDate)
-                .endDate(localEndDate).amount(amount).company(companyRepository.getById(companyId)).price(price).image(image).build();
-        ArrayList<Map<String, String>> map = new ArrayList<Map<String, String>>();
-        Map<String, String> m = new HashMap<String, String>();
-        try {
-            validations.validateCouponTitleExist(coupon.getTitle(), companyId);
-        } catch (CouponAlreadyExistsException e) {
-            System.err.println(e.getMessage());
-            m.put("ans", "false");
-            map.add(m);
-            return map;
-        }
-        Company company = companyRepository.findById(companyId).get();
-        company.addToCoupons(coupon);
-        companyRepository.save(company);
-        System.out.println("add "+coupon.toString());
-        m.put("ans", "true");
-        map.add(m);
-        return map;
+    
+    @GetMapping("getAllCompanies")
+    public ResponseEntity<?> getAllCompanies() 
+    {
+    	try
+    	{
+    		ObjectMapper mapper = new ObjectMapper();
+    		List<Company> companies= adminService.getAllCompanies();
+    	 
+    		String json = ObjectToJson.toJson(companies);
+    		
+    		return new ResponseEntity<String>(json, HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Unexpected error!", HttpStatus.NO_CONTENT);
+    	}    	
+    }
+    
+    @PostMapping("addCompany")
+    public ResponseEntity<?> addCompany(@RequestParam String name, @RequestParam String email, @RequestParam String password)
+    {
+    	Company newCompany = new Company();
+    	
+    	newCompany.setName(name);
+    	newCompany.setEmail(email);
+    	newCompany.setPassword(password);
+    	
+    	try
+    	{
+    		adminService.addCompany(newCompany);
+    		return new ResponseEntity<String>("Added company: "+newCompany.getName(), HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to add company", HttpStatus.BAD_REQUEST);
+    	}
     }
 
+    @PutMapping("updateCompany")
+    public ResponseEntity<?> updateCompany(@RequestParam int id, @RequestParam String name, @RequestParam String email, @RequestParam String password)
+    {
+    	try
+    	{
+    		Company company = adminService.getOneCompany(id);
+    		
+    		company.setName(name);
+    		company.setEmail(email);
+    		company.setPassword(password);
+    		
+    		adminService.updateCompany(company);
+    		
+    		return new ResponseEntity<String>("Updated company: "+company.getName(), HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to update company", HttpStatus.BAD_REQUEST);
+    	}
+    }
+    
+    @DeleteMapping("deleteCompany")
+    public ResponseEntity<?> deleteCompany(@RequestParam int id)
+    {
+    	try
+    	{
+    		adminService.deleteCompany(id);
+    		
+    		return new ResponseEntity<String>("Company was deleted successfully", HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to delete company", HttpStatus.BAD_REQUEST);
+    	}
+    }
+    
+    @GetMapping("getOneCompany")
+    public ResponseEntity<?> getOneCompany(int id) throws JsonProcessingException
+    {
+    	try
+    	{
+    		Company company =  adminService.getOneCompany(id);
 
-    @GetMapping("delete")
-    @ResponseBody
-    public ArrayList<Map<String, String>> deleteCoupon(@RequestParam int id) {
+    		String json = ObjectToJson.toJson(company);
+    		
+    		return new ResponseEntity<String>(json, HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to find company with provided id: "+id, HttpStatus.BAD_REQUEST);
+    	} 
+    }
 
-        System.out.println("delete "+"id:" + id);
+    @PostMapping("addCustomer")
+    public ResponseEntity<?> addCustomer(@RequestParam String firstName,@RequestParam String lastName ,@RequestParam String email, @RequestParam String password)
+    {
+    	Customer newCustomer = new Customer();
+    	
+    	newCustomer.setFirstName(firstName);
+    	newCustomer.setLastName(lastName);
+    	newCustomer.setEmail(email);
+    	newCustomer.setPassword(password);
+    	
+    	try
+    	{
+    		adminService.addCustomer(newCustomer);
+    		return new ResponseEntity<String>("Added customer: "+newCustomer.getFirstName()+" "+newCustomer.getLastName(), HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to add customer", HttpStatus.BAD_REQUEST);
+    	}
+    }
 
-        ArrayList<Map<String, String>> map = new ArrayList<Map<String, String>>();
-        Map<String, String> m = new HashMap<String, String>();
-        try {
-            validations.validateDeleteAllowed(id, companyId);
-        } catch (couponNotExistsException | companyNotOwnsCouponException e) {
-            System.err.println(e.getMessage());
-            m.put("ans", "false");
-            map.add(m);
-            return map;
-        }
-        Company company = companyRepository.getById(companyId);
-        Coupon coupon = couponRepository.findById(id).get();
+    @PutMapping("updateCustomer")
+    public ResponseEntity<?> updateCustomer(@RequestParam int id,@RequestParam String firstName,@RequestParam String lastName ,@RequestParam String email, @RequestParam String password)
+    {
+    	try
+    	{
+        	Customer customer = adminService.getOneCustomer(id);
+        	
+        	customer.setFirstName(firstName);
+        	customer.setLastName(lastName);
+        	customer.setEmail(email);
+        	customer.setPassword(password);
+    		
+    		adminService.updateCustomer(customer);
+    		
+    		return new ResponseEntity<String>("Updated company: "+customer.getFirstName()+" "+customer.getLastName(), HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to update customer", HttpStatus.BAD_REQUEST);
+    	}
+    }
+    
+    @DeleteMapping("deleteCustomer")
+    public ResponseEntity<?> deleteCustomer(@RequestParam int id)
+    {
+    	try
+    	{
+    		adminService.deleteCustomer(id);
+    		
+    		return new ResponseEntity<String>("Customer was deleted successfully", HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to delete customer", HttpStatus.BAD_REQUEST);
+    	}
+    }
+    
+    @GetMapping("getOneCustomer")
+    public ResponseEntity<?> getOneCustomer(int id) throws JsonProcessingException
+    {
+    	try
+    	{    	
+    		Customer customer =  adminService.getOneCustomer(id);
 
-        company.removeCoupon(coupon);
-        companyRepository.save(company);
-        couponRepository.delete(coupon);
-        m.put("ans", "true");
-        map.add(m);
-        return map;
-    }*/
+    		Customer newCustomer = new Customer();
+    		newCustomer.setId(customer.getId());
+    		newCustomer.setFirstName(customer.getFirstName());
+    		newCustomer.setLastName(customer.getLastName());
+    		newCustomer.setEmail(customer.getEmail());
+    		newCustomer.setPassword(customer.getPassword());
+    		
+    		String json = ObjectToJson.toJson(newCustomer);
 
+    		return new ResponseEntity<String>(json, HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Failed to find customer with provided id: "+id, HttpStatus.BAD_REQUEST);
+    	} 
+    }
+
+    @GetMapping("getAllCustomers")
+    public ResponseEntity<?> getAllCustomers() 
+    {
+    	try
+    	{
+    		List<Customer> customers = adminService.getAllCustomers();
+    	 
+    		String json = ObjectToJson.toJson(customers);
+    		
+    		return new ResponseEntity<String>(json, HttpStatus.OK);
+    	}
+    	
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Unexpected error!", HttpStatus.NO_CONTENT);
+    	}    	
+    }
 }
 
 
---
+++ cupon_server/src/main/java/com/evgenie_tomer_itay/controllers/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package com.evgenie_tomer_itay.controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration 
+{
+	// To run in browser: "http://localhost:3000/swagger-ui/#/"
+	// Don't forget the "#" symbol - it's important!
+	
+	@Bean
+	public Docket api()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+}
