8b38489f19ef1f291bdabb1f3d1070f5762116f1
TomerLiron <90131478+TomerLiron@users.noreply.github.com>
2022-01-22 17:06:34 +0200

    Add files via upload

--- cupon_server/src/main/java/com/evgenie_tomer_itay/controllers/Customer_controller.java
+++ cupon_server/src/main/java/com/evgenie_tomer_itay/controllers/Customer_controller.java
@@ -1,7 +1,13 @@
 package com.evgenie_tomer_itay.controllers;
 
+import com.evgenie_tomer_itay.entities.Category;
 import com.evgenie_tomer_itay.entities.Coupon;
+import com.evgenie_tomer_itay.entities.Customer;
+import com.evgenie_tomer_itay.exceptions.couponExceptions.couponExpiredException;
+import com.evgenie_tomer_itay.exceptions.couponExceptions.couponNotExistsException;
+import com.evgenie_tomer_itay.exceptions.couponExceptions.noCouponsLeftException;
 import com.evgenie_tomer_itay.exceptions.customerExceptions.CustomerAlreadyExistsException;
+import com.evgenie_tomer_itay.exceptions.customerExceptions.couponAlreadyPurchasedException;
 import com.evgenie_tomer_itay.services.CustomerServiceImplementation;
 import com.evgenie_tomer_itay.utilities.Credentials;
 import com.evgenie_tomer_itay.utilities.SimpleTokenManager;
@@ -43,84 +49,67 @@ public class Customer_controller {
             List<Coupon> coupons = customerService.getPurchasedCoupons();
             System.out.println("all " + coupons.size());
             ArrayList<Map<String, String>> Map = new ArrayList<Map<String, String>>();
-            //@RequestParam String category,@RequestParam String title,@RequestParam String description, @RequestParam int amount,
-            //@RequestParam String startDate, @RequestParam String endDate,@RequestParam double price,@RequestParam String image
-//            for (int i = 0; i < coupons.size(); i++) {
-//                Map<String, String> m = new HashMap<String, String>();
-//                m.put("id", "" + coupons.get(i).getId());
-//                m.put("company", "" + coupons.get(i).getCompany().getId());
-//                m.put("category", "" + coupons.get(i).getCategory());
-//                m.put("title", coupons.get(i).getTitle());
-//                m.put("description", coupons.get(i).getDescription());
-//                m.put("amount", "" + coupons.get(i).getAmount());
-//                m.put("startDate", "" + coupons.get(i).getStartDate());
-//                m.put("endDate", "" + coupons.get(i).getEndDate());
-//                m.put("price", "" + coupons.get(i).getPrice());
-//                m.put("image", coupons.get(i).getImage());
-//                Map.add(m);
-//            }
+            return new ResponseEntity<List<Coupon>>(coupons, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
+    }//getCouponsByPrice
+
+    @PostMapping("/getCouponsByPrice")
+    public ResponseEntity<?> getCouponsByPrice(@RequestBody double price, @RequestHeader("token") String token) {
+        System.out.println("price " + price + " token:" + token);
+        if (simpleTokenManager.isTokenExist(token)) {
+            System.out.println("good");
+            List<Coupon> coupons = customerService.getCouponsByPrice(price);
+            System.out.println("all " + coupons.size());
+            ArrayList<Map<String, String>> Map = new ArrayList<Map<String, String>>();
             return new ResponseEntity<List<Coupon>>(coupons, HttpStatus.OK);
         }
         return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
     }
+
+    //spring security,token
+    @PostMapping("/getCouponsByCategory")
+    public ResponseEntity<?> getCouponsByCategory(@RequestBody String category, @RequestHeader("token") String token) {
+        Category sendCategory = Category.valueOf(category);
+        System.out.println("Category " + sendCategory + " token:" + token);
+        if (simpleTokenManager.isTokenExist(token)) {
+            System.out.println("good");
+            List<Coupon> coupons = customerService.getCouponsByCategory(Category.valueOf(category));
+            System.out.println("all " + coupons.size());
+            ArrayList<Map<String, String>> Map = new ArrayList<Map<String, String>>();
+            return new ResponseEntity<List<Coupon>>(coupons, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
+    }
+
+
+    @PostMapping("/purchaseCoupon")
+    public ResponseEntity<?> purchaseCoupon(@RequestBody int id, @RequestHeader("token") String token) {
+        System.out.println("purchaseCoupon");
+        if (simpleTokenManager.isTokenExist(token)) {
+            System.out.println("good");
+            try {
+
+                customerService.purchaseCoupon(id);
+
+                return new ResponseEntity<String>("OK", HttpStatus.OK);
+            } catch (couponNotExistsException | noCouponsLeftException | couponAlreadyPurchasedException | couponExpiredException e) {
+                System.out.println(e.getMessage());
+//                String message=e.getMessage();
+                return new ResponseEntity<String>(e.getMessage(), HttpStatus.ACCEPTED);
+            }
+        }
+        return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/getCustomerDetails")
+    public ResponseEntity<?> getCustomerDetails(@RequestHeader("token") String token) {
+        System.out.println("purchaseCoupon");
+        if (simpleTokenManager.isTokenExist(token)) {
+            System.out.println("good");
+            Customer customer=customerService.getCustomerDetails();
+            return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
+    }
 }
-  /*
-    @GetMapping("add")
-    @ResponseBody
-    public ArrayList<Map<String, String>> addCoupon(@RequestParam String category, @RequestParam String title, @RequestParam String description, @RequestParam int amount,
-                                                    @RequestParam String startDate, @RequestParam String endDate, @RequestParam double price, @RequestParam String image) {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        LocalDate localStartDate = LocalDate.parse(startDate, formatter);
-        LocalDate localEndDate = LocalDate.parse(endDate, formatter);
-
-        Coupon coupon = Coupon.builder().category(Category.valueOf(category)).title(title).description(description).startDate(localStartDate)
-                .endDate(localEndDate).amount(amount).company(companyRepository.getById(companyId)).price(price).image(image).build();
-        ArrayList<Map<String, String>> map = new ArrayList<Map<String, String>>();
-        Map<String, String> m = new HashMap<String, String>();
-        try {
-            validations.validateCouponTitleExist(coupon.getTitle(), companyId);
-        } catch (CouponAlreadyExistsException e) {
-            System.err.println(e.getMessage());
-            m.put("ans", "false");
-            map.add(m);
-            return map;
-        }
-        Company company = companyRepository.findById(companyId).get();
-        company.addToCoupons(coupon);
-        companyRepository.save(company);
-        System.out.println("add "+coupon.toString());
-        m.put("ans", "true");
-        map.add(m);
-        return map;
-    }
-
-
-    @GetMapping("delete")
-    @ResponseBody
-    public ArrayList<Map<String, String>> deleteCoupon(@RequestParam int id) {
-
-        System.out.println("delete "+"id:" + id);
-
-        ArrayList<Map<String, String>> map = new ArrayList<Map<String, String>>();
-        Map<String, String> m = new HashMap<String, String>();
-        try {
-            validations.validateDeleteAllowed(id, companyId);
-        } catch (couponNotExistsException | companyNotOwnsCouponException e) {
-            System.err.println(e.getMessage());
-            m.put("ans", "false");
-            map.add(m);
-            return map;
-        }
-        Company company = companyRepository.getById(companyId);
-        Coupon coupon = couponRepository.findById(id).get();
-
-        company.removeCoupon(coupon);
-        companyRepository.save(company);
-        couponRepository.delete(coupon);
-        m.put("ans", "true");
-        map.add(m);
-        return map;
-    }
-*/
