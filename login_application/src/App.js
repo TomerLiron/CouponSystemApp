f75a142353ea627b913bbd22e478fc388d2dac38
TomerLiron <90131478+TomerLiron@users.noreply.github.com>
2022-01-29 15:01:34 +0200

    Add files via upload

--- login_application/src/App.js
+++ login_application/src/App.js
@@ -1,18 +1,16 @@
-import React, { useEffect, Fragment } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-// import Nav from './Nav';
+import React, { useEffect, } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { authActions } from './store/auth';
 import { UserActions } from './store/User';
 
-import CompanyLogin from './components/CompanyLogin/CompanyLogin';
-import CustomerLogin from './components/CustomerLogin/CustomerLogin';
-import AdminLogin from './components/AdminLogin/AdminLogin';
+import CustomerLogin from './components/Login/Login';
 import CompanyHome from './components/CompanyHome/CompanyHome';
 import AdminHome from './components/AdminHome/AdminHome';
 import CustomerHome from './components/CustomerHome/CustomerHome';
 import MainHeader from './components/MainHeader/MainHeader';
+import {  StyledEngineProvider } from '@mui/material';
 
-function App() {
+function App(props) {
   const dispatch = useDispatch();
   const isAuth = useSelector(state => state.auth.isAuthenticated);
   const User = useSelector((state) => state.User.User);
@@ -20,34 +18,30 @@ function App() {
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
     const storedUserTokenInInformation = localStorage.getItem('token');
-    const storedUserUserInformation=localStorage.getItem('user');
+    const storedUserUserInformation = localStorage.getItem('user');
     if (storedUserLoggedInInformation === "1") {
       dispatch(authActions.login(storedUserTokenInInformation));
     }
-    console.log(storedUserTokenInInformation);
-    if(storedUserUserInformation==="1"){
+    if (storedUserUserInformation === "1") {
       dispatch(UserActions.company());
-    }else if(storedUserUserInformation==="2"){
+    } else if (storedUserUserInformation === "2") {
       dispatch(UserActions.customer());
-    }else{
+    } else {
       dispatch(UserActions.admin());
 
     }
   }, [dispatch]);
 
   return (
-    <Fragment>
-
+    <StyledEngineProvider injectFirst>
+    <main>
       <MainHeader />
-      <main>
-        {!isAuth&&User==="company" && <CompanyLogin />}
-        {!isAuth&&User==="customer" && <CustomerLogin />}
-        {!isAuth&&User==="admin" && <AdminLogin/>}
-        {isAuth &&User==="company"&& <CompanyHome />}
-        {isAuth &&User==="customer"&& <CustomerHome />}
-        {isAuth &&User==="admin"&& <AdminHome />}  
+        {!isAuth && <CustomerLogin />}
+        {isAuth && User === "company" && <CompanyHome />}
+        {isAuth && User === "customer" && <CustomerHome />}
+        {isAuth && User === "admin" && <AdminHome />}
       </main>
-    </Fragment>
+      </StyledEngineProvider>
   );
 }
 
