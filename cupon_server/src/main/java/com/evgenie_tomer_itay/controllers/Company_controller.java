f0fd96e73167077a0c00e3194b94d3ea6c466a42
ItayMz <79039985+ItayMz@users.noreply.github.com>
2022-01-31 14:37:42 +0200

    Add files via upload

--- cupon_server/src/main/java/com/evgenie_tomer_itay/controllers/Company_controller.java
+++ cupon_server/src/main/java/com/evgenie_tomer_itay/controllers/Company_controller.java
@@ -38,8 +38,10 @@ public class Company_controller {
 		}
 	}
 
-	@PostMapping("addCoupon/{token}")
-	public ResponseEntity<?> addCoupon(@RequestBody Coupon coupon, @PathVariable String token) {
+	@PostMapping("addCoupon")
+	public ResponseEntity<?> addCoupon(@RequestBody Coupon coupon, @RequestHeader String token) {
+		System.err.println(coupon);
+		System.out.println("ADD REQUEST");
 		if (simpleTokenManager.isTokenExist(token)) {
 			System.out.println("Token approved");
 			try {
@@ -52,10 +54,13 @@ public class Company_controller {
 		return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
 	}
 
-	@PutMapping("updateCoupon/{token}")
-	public ResponseEntity<?> updateCoupon(@RequestBody Coupon coupon, @PathVariable String token) {
+	@PutMapping("updateCoupon")
+	public ResponseEntity<?> updateCoupon(@RequestBody Coupon coupon, @RequestHeader String token) {
+		System.err.println(coupon);
 		if (simpleTokenManager.isTokenExist(token)) {
 			System.out.println("Token approved");
+			System.out.println("UPDATE REQUEST");
+
 			try {
 				companyService.updateCoupon(coupon);
 			} catch (couponNotExistsException | CouponAlreadyExistsException | companyNotOwnsCouponException e) {
@@ -66,12 +71,13 @@ public class Company_controller {
 		return new ResponseEntity<String>("No session!", HttpStatus.BAD_REQUEST);
 	}
 
-	@DeleteMapping("deleteCoupon/{token}")
-	public ResponseEntity<?> deleteCoupon(@RequestParam int couponId, @PathVariable String token) {
+	@DeleteMapping("deleteCoupon")
+	public ResponseEntity<?> deleteCoupon(@RequestParam int id, @RequestHeader String token) {
+		System.out.println("DELETE REQUEST");
 		if (simpleTokenManager.isTokenExist(token)) {
 			System.out.println("Token approved");
 			try {
-				companyService.deleteCoupon(couponId);
+				companyService.deleteCoupon(id);
 			} catch (couponNotExistsException | companyNotOwnsCouponException e) {
 				return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
 			}
@@ -82,114 +88,50 @@ public class Company_controller {
 	}
 
 	// spring security,token
-	@GetMapping("allCoupons/{token}")
-	@ResponseBody
-	public ResponseEntity<?> getAllCoupons(@PathVariable String token) {
-		System.out.println("all");
+	@GetMapping("allCoupons")
+	public ResponseEntity<?> getAllCoupons(@RequestHeader String token) {
+		System.out.println("GOT A GET_ALL REQUEST FROM CLIENT");
 		if (simpleTokenManager.isTokenExist(token)) {
-			System.out.println("good");
+			System.out.println("Token Accepted");
 			List<Coupon> coupons = companyService.getAllCoupons();
-			ArrayList<Map<String, String>> couponsList = new ArrayList<Map<String, String>>();
-			for (int i = 0; i < coupons.size(); i++) {
-				Map<String, String> map = new HashMap<String, String>();
-				map.put("id", "" + coupons.get(i).getId());
-				map.put("company", "" + coupons.get(i).getCompany().getId());
-				map.put("category", "" + coupons.get(i).getCategory());
-				map.put("title", coupons.get(i).getTitle());
-				map.put("description", coupons.get(i).getDescription());
-				map.put("amount", "" + coupons.get(i).getAmount());
-				map.put("startDate", "" + coupons.get(i).getStartDate());
-				map.put("endDate", "" + coupons.get(i).getEndDate());
-				map.put("price", "" + coupons.get(i).getPrice());
-				map.put("image", coupons.get(i).getImage());
-				couponsList.add(map);
-			}
-			return new ResponseEntity<ArrayList<Map<String, String>>>(couponsList, HttpStatus.OK);
+		
+			return new ResponseEntity<>(coupons, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
 
 	}
-	@GetMapping("getByCategory/{token}")
-	public ResponseEntity<?>getCouponsByCategory(Category category, @PathVariable String token){
-		if(simpleTokenManager.isTokenExist(token)) {
+
+	@GetMapping("getByCategory")
+	public ResponseEntity<?> getCouponsByCategory(@RequestParam Category category, @RequestHeader String token) {
+		if (simpleTokenManager.isTokenExist(token)) {
 			System.out.println("Token approved");
+		
+
 			List<Coupon> filteredCoupons = companyService.getCouponsByCategory(category);
-			return new ResponseEntity<>(filteredCoupons,HttpStatus.OK);
-		}
-		return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
-
-	}
-	
-	@GetMapping("getByPrice/{token}")
-	public ResponseEntity<?>getCouponsByPrice(@RequestParam int maxPrice, @PathVariable String token){
-		if(simpleTokenManager.isTokenExist(token)) {
-			System.out.println("Token approved");
-			return new ResponseEntity<>(companyService.getCouponsByMaximumPrice(maxPrice),HttpStatus.OK);
+			return new ResponseEntity<>(filteredCoupons, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
 
 	}
 
-	@GetMapping("getDetails/{token}")
-	public ResponseEntity<?> getCompanyDetails(@PathVariable String token) {
-		if(simpleTokenManager.isTokenExist(token)) {
+	@GetMapping("getByPrice")
+	public ResponseEntity<?> getCouponsByPrice(@RequestParam int price, @RequestHeader String token) {
+		if (simpleTokenManager.isTokenExist(token)) {
 			System.out.println("Token approved");
-			return new ResponseEntity<>(companyService.companyDetails(),HttpStatus.OK);
+			List<Coupon> filteredCoupons = companyService.getCouponsByMaximumPrice(price);
+			return new ResponseEntity<List<Coupon>>(filteredCoupons, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
+
+	}
+
+	@GetMapping("getDetails")
+	public ResponseEntity<?> getCompanyDetails(@RequestHeader String token) {
+		if (simpleTokenManager.isTokenExist(token)) {
+			System.out.println("Token approved");
+			return new ResponseEntity<>(companyService.companyDetails(), HttpStatus.OK);
 		}
 		return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST);
 
 	}
 }
-
-/*
- * System.out.println("all " + coupons.size()); //@RequestParam String
- * category,@RequestParam String title,@RequestParam String
- * description, @RequestParam int amount, //@RequestParam String
- * startDate, @RequestParam String endDate,@RequestParam double
- * price,@RequestParam String image
- * 
- * 
- * /*
- * 
- * @PostMapping("/addCoupon") ResponseEntity<?> addCoupon(@RequestBody Coupon
- * coupon, @RequestHeader("token") String token) {
- * 
- * if (simpleTokenManager.isTokenExist(token)) { try {
- * validations.validateCouponTitleExist(coupon.getTitle(), companyId); } catch
- * (CouponAlreadyExistsException e) { System.err.println(e.getMessage());
- * 
- * } Company company = companyRepository.findById(companyId).get();
- * company.addToCoupons(coupon); companyRepository.save(company);
- * System.out.println("add "+coupon.toString());
- * 
- * Integer id = coupon.getId();
- * System.out.println("Got a new coupon: "+coupon+", token="+token); return new
- * ResponseEntity<Integer>(id, HttpStatus.OK); } else {
- * System.out.println("Got a new coupon: not" );
- * 
- * return new ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST); }
- * // } // System.out.println("Got a new coupon: "+coupon); // return new
- * ResponseEntity<String>("No Session!", HttpStatus.BAD_REQUEST); }
- * 
- * 
- * 
- * 
- * @GetMapping("delete")
- * 
- * @ResponseBody public ArrayList<Map<String, String>>
- * deleteCoupon(@RequestParam int id) {
- * 
- * System.out.println("delete " + "id:" + id);
- * 
- * ArrayList<Map<String, String>> map = new ArrayList<Map<String, String>>();
- * Map<String, String> m = new HashMap<String, String>(); try {
- * validations.validateDeleteAllowed(id, companyId); } catch
- * (couponNotExistsException | companyNotOwnsCouponException e) {
- * System.err.println(e.getMessage()); m.put("ans", "false"); map.add(m); return
- * map; } Company company = companyRepository.getById(companyId); Coupon coupon
- * = couponRepository.findById(id).get();
- * 
- * company.removeCoupon(coupon); companyRepository.save(company);
- * couponRepository.delete(coupon); m.put("ans", "true"); map.add(m); return
- * map; }
- */
