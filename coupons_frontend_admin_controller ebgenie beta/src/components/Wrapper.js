3f4b613bc51f045b7bcdf99831f8b0f4b7546fa6
TomerLiron <90131478+TomerLiron@users.noreply.github.com>
2022-02-01 18:22:39 +0200

    Delete coupons_frontend_admin_controller ebgenie beta directory

--- coupons_frontend_admin_controller ebgenie beta/src/components/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Wrapper(props) {
-    const { children, className } = props;
-    return <div className={className}>{children}</div>;
-  }
-  
-  export default Wrapper;
-  
