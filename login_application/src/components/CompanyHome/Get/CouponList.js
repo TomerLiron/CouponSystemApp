48138cbbb48729b56050e545e2be9b00065b89c6
TomerLiron <90131478+TomerLiron@users.noreply.github.com>
2022-02-03 18:52:08 +0200

    Add files via upload

--- login_application/src/components/CompanyHome/Get/CouponList.js
+++ login_application/src/components/CompanyHome/Get/CouponList.js
@@ -1,28 +1,27 @@
-import React,{Fragment} from 'react';
-import DeleteCoupon from '../Delete/DeleteCoupon';
-
-import Coupon from './Coupon';
-import classes from './CouponList.module.css';
+import React, { useState, Fragment } from 'react';
 import {
-  Avatar,
-  // BottomNavigation,
+  Avatar, Button,
   Box,
   Card,
-  // Checkbox,
   Table,
   TableBody,
   TableCell,
   TableHead,
-  // TablePagination,
   TableRow,
+  TableContainer,
   Typography
 } from '@mui/material';
-import { getInitials } from '../../AdminHome/GetCompany/get-initials';
+import DeleteCoupon from '../Delete/DeleteCoupon'
+
+import Paper from '@mui/material/Paper';
+import Coupon from './Coupon';
 
 const CouponList = (props) => {
+  const [edit, setEdit] = useState(false);
+
   return (
  
-    <div className={classes['movies-list']}>
+    <div >
       <Card >
       <Box sx={{ minWidth: 1050 }}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table" >
