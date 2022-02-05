18011ebc8b4dc8629a9a4ac679895d5d46f4d447
TomerLiron <90131478+TomerLiron@users.noreply.github.com>
2022-02-05 21:28:25 +0200

    Add files via upload

--- cupon_server/src/main/java/com/evgenie_tomer_itay/CouponApplication.java
+++ cupon_server/src/main/java/com/evgenie_tomer_itay/CouponApplication.java
@@ -2,6 +2,7 @@ package com.evgenie_tomer_itay;
 
  import com.evgenie_tomer_itay.entities.Customer;
  import com.evgenie_tomer_itay.services.CustomerServiceImplementation;
+ import com.evgenie_tomer_itay.utilities.SimpleTokenManager;
  import org.springframework.boot.SpringApplication;
  import org.springframework.boot.autoconfigure.SpringBootApplication;
  import org.springframework.context.ConfigurableApplicationContext;
@@ -12,14 +13,8 @@ public class CouponApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx =SpringApplication.run(CouponApplication.class, args);
 		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		CustomerServiceImplementation serviceImplementation = ctx.getBean(CustomerServiceImplementation.class);
-		serviceImplementation.purchaseCoupon(10);
-//		LocalDate localDate= LocalDate.of(2002,3,6);
-//		System.out.println(localDate);
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-//		String date ="2021-12-12";
-//		LocalDate localDate = LocalDate.parse(date, formatter);
-//		System.out.println(localDate);
+		SimpleTokenManager simpleTokenManager = ctx.getBean(SimpleTokenManager.class);
+		simpleTokenManager.initThread();
 		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
 
