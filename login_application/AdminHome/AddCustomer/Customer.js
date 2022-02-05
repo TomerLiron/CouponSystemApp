479652cd166c928b5f50795baef21f70b7809ba7
TomerLiron <90131478+TomerLiron@users.noreply.github.com>
2022-02-05 21:16:01 +0200

    Add files via upload

---
+++ login_application/AdminHome/AddCustomer/Customer.js
@@ -0,0 +1,80 @@
+import { useCallback, useRef } from 'react';
+import './AddCustomer';
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from '../../../store/auth';
+
+const Customer = (props) => {
+    const token = useSelector(state => state.auth.token);
+    const dispatch = useDispatch();
+    const firstNameRef = useRef("");
+    const lastNameRef = useRef("");
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+
+    const submitHandler = useCallback(async (event) => {
+        event.preventDefault();
+        console.log(categoryValue)
+        const company = {
+            firstName: firstNameRef.current.value,
+            lastName: lastNameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        };
+
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json", token: token },
+            body: JSON.stringify(company),
+        };
+        try {
+            console.log("company:" + JSON.stringify(company));
+
+            const response = await fetch("/admin/addCustomer/", requestOptions);
+            if (!response.ok) {
+                window.alert("Session timeout!");
+                dispatch(authActions.logout());
+                throw new Error("Something went wrong!");
+            } else if (response.status === 202) {
+                window.alert(await response.text())
+                props.stopEditingHandler();
+            }else{
+                console.log("Response Okay!");
+                const id = await response.text();
+                const CustomerWithId = { "id": id, ...company };
+                props.onAddCustomer(CustomerWithId);    
+                props.stopEditingHandler();
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, [props, token, dispatch]);
+    const categoryValue = (value) => { return value }
+
+    return (
+        <form id="form" onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label htmlFor="name">First Name</label>
+                    <input type="text" id="firstName" ref={firstNameRef} />
+                </div>
+                <div className='new-expense__control'>
+                    <label htmlFor="name">Last Name</label>
+                    <input type="text" id="lastName" ref={lastNameRef} />
+                </div>
+                <div className='new-expense__control'>
+                    <label htmlFor="email">email</label>
+                    <input type="text" id="email" ref={emailRef} />
+                </div>
+                <div className='new-expense__control'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" ref={passwordRef} />
+                </div>
+                <div className='new-expense__actions'>
+                    <button onClick={submitHandler} type='submit'>Add Expense</button>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default Customer;
---
+++ login_application/AdminHome/GetCompany/CompanyList.js
@@ -0,0 +1,89 @@
+import React, { Fragment } from 'react';
+import UpdateCompany from '../UpdateCompany/UpdateCompany'
+// import DeleteCustomer from '../DeleteCustomer/DeleteCompany'
+import DeleteCustomer from '../DeleteCustomer/DeleteCompany'
+
+import {
+  Avatar,
+  Box,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography
+} from '@mui/material';
+import { getInitials } from './get-initials';
+
+const CompanyList = (props) => {
+  return (
+    <Card >
+      <Box sx={{ minWidth: 1050 }}>
+
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table" >
+          <TableHead >
+            <TableRow >
+              <TableCell align='left'>
+                id
+              </TableCell>
+              <TableCell align='left'>
+                Name
+              </TableCell>
+              <TableCell align='left'>
+                Email
+              </TableCell>
+              <TableCell align='left'>
+
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.companys.map((company) => (
+              <Fragment key={company.id}>
+                <TableRow
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+
+                >
+                  <TableCell style={{ width: '10%' }}>
+                    {company.id}
+                  </TableCell>
+                  <TableCell style={{ width: '35%' }}>
+                    <Box
+                      sx={{
+                        alignItems: 'center',
+                        display: 'flex'
+                      }}
+                    >
+                      <Avatar
+                        src={company.avatarUrl}
+                        sx={{ mr: 2 }}
+                      >
+                        {getInitials(company.name)}
+                      </Avatar>
+                      <Typography
+                        color="textPrimary"
+                        variant="body1"
+                      >
+                        {company.name}
+                      </Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell style={{ width: '30%' }}>
+                    {company.email}
+                  </TableCell>
+                  <TableCell style={{ width: '50%' }}>
+                    <UpdateCompany company={company} updateFieldChanged={props.updateFieldChanged} />
+                    <DeleteCustomer company={company} removeHandler={props.removeHandler} />
+                  </TableCell>
+                </TableRow>
+              </Fragment>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </Card>
+  );
+};
+
+export default CompanyList;
