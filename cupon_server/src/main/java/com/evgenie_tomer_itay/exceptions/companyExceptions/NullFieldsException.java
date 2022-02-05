c793b5b72198d2fcc3b2b7cd80ae2f73f88d180f
TomerLiron <90131478+TomerLiron@users.noreply.github.com>
2022-02-05 21:47:59 +0200

    Delete cupon_server directory

--- cupon_server/src/main/java/com/evgenie_tomer_itay/exceptions/companyExceptions/NullFieldsException.java
+++
@@ -1,7 +0,0 @@
-package com.evgenie_tomer_itay.exceptions.companyExceptions;
-
-public class NullFieldsException extends Exception {
-    public NullFieldsException(String message){
-        super(message);
-    }
-}
--- cupon_server/src/main/java/com/evgenie_tomer_itay/services/ClientType.java
+++
@@ -1,7 +0,0 @@
-package com.evgenie_tomer_itay.services;
-
-public enum ClientType
-{
-	ADMINISTRATOR, COMPANY, CUSTOMER;
-
-}
